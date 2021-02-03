8faaa7d23e5a0cafbc3c5c40c51aab1301054357
Tim Yarmatov <yarmatoff@yandex.ru>
2021-02-03 07:41:06 +0300

    Исправление замечаний - задание 2:

--- js/main.js
+++ js/main.js
@@ -2,20 +2,27 @@
 
 //За основу взят пример из источника https://learn.javascript.ru/task/random-int-min-max
 
-function randomInteger(min, max) {
+const chooseNumber = (min, max) => {
   // случайное число от min до (max+1)
-  if (min >= 0 && min < max) {
-    let rand = min + Math.random() * (max + 1 - min);
+  if ((min >= 0 && min < max) || (min === max)) {
+    const rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
   console.log(' Неккоректно указан диапазон чисел.');
 }
 
+const chooseFromBig = (min, max) => {
+  if (min > max) {
+    return Math.floor(max + Math.random() * (min + 1 - max));
+  }
+  else chooseNumber(min, max);
+}
+
 //ниже будет код cтрелочной функции checkMaximumLength
 
 const checkMaximumLength = (checkedString, maxLength) => {
-  if (maxLength <= 140 && checkedString.length <= maxLength) {
-    document.write(checkedString);
+  if (checkedString.length <= maxLength) {
+    return checkedString;
   }
-  console.log(' Строка не проходит по длинне: в ней больше 140 символов!');
+  console.log(' Строка не проходит по длинне');
 };
